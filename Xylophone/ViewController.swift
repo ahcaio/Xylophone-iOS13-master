f0a1ca397aaf39929be1749821459c88fd7ad91e
Caio Cesar <ahcaio@hotmail.com>
2022-11-20 20:27:33 -0300

    Add sounds and opacity change

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,15 +5,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(nameOfSound: sender.currentTitle!)
+        //Reduces the sender's (the button that got pressed) opacity to half.
+          sender.alpha = 0.5
 
+          //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+    }
     
-
+    func playSound(nameOfSound: String) {
+        let url = Bundle.main.url(forResource: nameOfSound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
